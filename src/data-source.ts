fe2145b4c0efd3aa75a1a44b2f38477f3f555e44
joyahmed <joy_ahmed_007@yahoo.com>
2025-05-24 13:46:00 +0600

    ✅Migration

---
+++ src/data-source.ts
@@ -0,0 +1,14 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'pass123',
+  database: 'postgres',
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  synchronize: false,
+});
--- src/events/entities/event.entity.ts.ts
+++ src/events/entities/event.entity.ts.ts
@@ -1,6 +1,5 @@
 import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
 
-@Index(['name', 'type'])
 @Entity()
 export class Event {
   @PrimaryGeneratedColumn()
