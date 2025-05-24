ecbec47b8d7072eb857d2200215a875e029b273a
joyahmed <joy_ahmed_007@yahoo.com>
2025-05-24 11:47:08 +0600

    ✅Pagination

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Repository } from 'typeorm';
 import { CreateCoffeDto } from './dto/create-coffe.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
@@ -20,9 +21,12 @@ export class CoffeesService {
     throw new NotFoundException(`Coffee #${id} not found`);
   }
 
-  findAll() {
+  findAll(paginationQuery: PaginationQueryDto) {
+    const { limit, offset } = paginationQuery;
     return this.coffeeRepository.find({
-      relations: ['flavors']
+      relations: ['flavors'],
+      skip: offset,
+      take: limit
     });
   }
 
