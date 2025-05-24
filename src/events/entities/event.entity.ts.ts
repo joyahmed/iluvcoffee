779e27c39631d6cbf9803205fe29396f4aaee008
joyahmed <joy_ahmed_007@yahoo.com>
2025-05-24 12:02:13 +0600

    ✅Index

--- src/events/entities/event.entity.ts.ts
+++ src/events/entities/event.entity.ts.ts
@@ -1,10 +1,12 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
 
+@Index(['name', 'type'])
 @Entity()
 export class Event {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Index()
   @Column()
   type: string;
 
